ff6b7bd563941134b0118e5a0774dd0c904de8c2
koichi nagata <reguluskoichi725@gmail.com>
2023-03-30 22:25:05 +0900

    プロジェクト作成

---
+++ repaymentSimulator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  repaymentSimulator
+//
+//  Created by 永田光一 on 2023/03/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
