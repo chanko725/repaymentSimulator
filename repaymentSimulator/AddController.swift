9d34c03064f2300f5ab2977d5cd5457ab96ed1c9
koichi nagata <reguluskoichi725@gmail.com>
2023-04-01 20:58:14 +0900

    データ登録画面の作成

---
+++ repaymentSimulator/AddController.swift
@@ -0,0 +1,39 @@
+//
+//  AddController.swift
+//  repaymentSimulator
+//
+//  Created by 永田光一 on 2023/04/01.
+//
+
+import UIKit
+
+//支払いに関するデータを格納する
+var paymentData = [String]()
+
+class AddController: UIViewController {
+    
+    //名称
+    @IBOutlet weak var paymentName: UITextField!
+    //支払い総額
+    //支払い方式
+    //年利
+    //遅延損害金の利率
+    //月毎の返済日
+    //月毎の返済額
+    //月毎の返済日
+    //ボーナス月の返済額
+    //ボーナス月の返済日
+    
+    @IBAction func addPayment(_ sender: Any) {
+        //配列に入力内容を格納
+        paymentData.append(paymentName.text!)
+        //追加後にフィールドを空にする
+        paymentName.text = ""
+        //UDに保存
+        UserDefaults.standard.set(paymentData, forKey: "paymentData")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- repaymentSimulator/ViewController.swift
+++ repaymentSimulator/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    //リストの表示数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return paymentData.count
+    }
+    
+    //リストの表示内容
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let paymentCell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "paymentCell", for: indexPath)
+        paymentCell.textLabel!.text = paymentData[indexPath.row]
+        return paymentCell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //追加画面で入力した内容を取得
+        if UserDefaults.standard.object(forKey: "paymentData") != nil {
+            paymentData = UserDefaults.standard.object(forKey: "paymentData") as! [String]
+        }
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
 
